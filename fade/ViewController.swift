e876d9be5cd05802700f0812f09020692941b29a
Marcus Smith <marcus.smith@stablekernel.com>
2017-03-21 10:34:05 -0400

    Adjust layer for rotation

--- fade/ViewController.swift
+++ fade/ViewController.swift
@@ -1,14 +1,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    private var gradient: CAGradientLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let gradient = CAGradientLayer()
+        gradient = CAGradientLayer()
         gradient.frame = view.bounds
         gradient.colors = [UIColor.red.cgColor, UIColor.purple.cgColor, UIColor.purple.cgColor, UIColor.blue.cgColor]
         gradient.locations = [0, 0.1, 0.9, 1]
         view.layer.addSublayer(gradient)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        gradient.frame = view.bounds
     }
 }
 
