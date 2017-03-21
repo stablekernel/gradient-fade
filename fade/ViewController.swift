84cc6744458cbf9c1f30b416629c22376e23733d
Marcus Smith <marcus.smith@stablekernel.com>
2017-03-21 10:44:11 -0400

    Move fade to label

--- fade/ViewController.swift
+++ fade/ViewController.swift
@@ -1,22 +1,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var label: UILabel!
     private var gradient: CAGradientLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         gradient = CAGradientLayer()
-        gradient.frame = view.bounds
+        gradient.frame = label.bounds
         gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor, UIColor.black.cgColor, UIColor.clear.cgColor]
         gradient.locations = [0, 0.1, 0.9, 1]
-        view.layer.mask = gradient
+        label.layer.mask = gradient
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        gradient.frame = view.bounds
+        gradient.frame = label.bounds
     }
 }
 
