0a422ca6d65b5c0b408967a6ae01fc02a8917bf1
Marcus Smith <marcus.smith@stablekernel.com>
2017-03-21 11:40:32 -0400

    Add fade to scroll view

--- fade/ViewController.swift
+++ fade/ViewController.swift
@@ -1,23 +1,45 @@
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate, CALayerDelegate {
+    @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var label: UILabel!
+    
     private var gradient: CAGradientLayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         gradient = CAGradientLayer()
-        gradient.frame = label.bounds
         gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor, UIColor.black.cgColor, UIColor.clear.cgColor]
         gradient.locations = [0, 0.1, 0.9, 1]
-        label.layer.mask = gradient
+        gradient.delegate = self
+        scrollView.layer.mask = gradient
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
-        gradient.frame = label.bounds
+        updateGradientFrame()
+    }
+    
+    // MARK: - UIScrollViewDelegate
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        updateGradientFrame()
+    }
+    
+    // MARK: - CALayerDelegate
+    func action(for layer: CALayer, forKey event: String) -> CAAction? {
+        return NSNull()
+    }
+    
+    // MARK: - Convenience
+    private func updateGradientFrame() {
+        gradient.frame = CGRect(
+            x: 0,
+            y: scrollView.contentOffset.y,
+            width: scrollView.bounds.width,
+            height: scrollView.bounds.height
+        )
     }
 }
 
